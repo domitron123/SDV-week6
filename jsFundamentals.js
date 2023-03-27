dcd8d327857e5b6bd1600c26c37ef21187c716b0
domitron123 <dom-townshend@live.nmit.ac.nz>
2023-03-27 13:14:20 +1300

    Week 6 variables

---
+++ jsFundamentals.js
@@ -0,0 +1,8 @@
+const fullName = 'Dom Townshend'
+console.log(typeof fullName)
+let age = 18
+console.log(typeof age)
+var address = '461 high stree Motuekea'
+console.log(typeof address)
+
+console.log(fullName + ' is ' + age + ' and lives in ' + address)
